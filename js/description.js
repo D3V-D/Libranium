1fc316490b66d8088ce24f343d73a85ece940242
D3V-D <devendangi10@gmail.com>
2024-05-07 08:13:06 -0400

    Added LLM interface with Azure to gen books by description

---
+++ js/description.js
@@ -0,0 +1,58 @@
+document.getElementById("generate-button").addEventListener("click", function() {
+    const textarea = document.querySelector("textarea")
+
+    if (!textarea.value ) {
+        alert("Description cannot be empty!")
+        return
+    }
+
+    if (textarea.value.length > 500) {
+        alert("Description is too long!")
+        return
+    }
+
+    if (textarea.value.length < 10) {
+        alert("Please use a longer description for better results.")
+        return
+    }
+
+    generate(textarea.value)
+})
+
+async function generate(description) {
+    document.getElementById("loader").style.display = "flex"
+
+    fetch('../.netlify/functions/infer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            textInput: description
+        })
+    }).then(response => {
+        if (!response.ok) {
+            console.error(response)
+        }
+        return response.json()
+    }).then(data => {
+        // handle
+        displayResponse(data)
+    }).catch(error => {
+        console.error(error)
+    })
+}
+
+function displayResponse(data) {
+    document.getElementById("loader").style.display = "none"
+
+    document.getElementById("results-overlay").style.display = "flex"
+    document.getElementById("result-text").innerText = data.choices[0].message.content
+}
+
+
+document.getElementById("close-overlay").addEventListener("click", () => {
+    document.getElementById("results-overlay").style.display = "none"
+    document.getElementById("results-text").innerText = ""
+})
+  
---
+++ netlify/functions/infer.mjs
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch'
+
+export const handler = async function (event, context) {
+    try {
+        const apiURL = process.env.ENDPOINT;
+        const apiKey = process.env.KEY;
+
+        const description = JSON.parse(event.body).textInput
+
+        const payload = JSON.stringify({
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant who recommends books based on a description provided. If the prompt is off-topic and doesn't request a book, you will politely refuse to answer."
+                },
+                {
+                    "role": "user",
+                    "content": "Please recommend me a book to read based on this description (there will be no follow up messages, so recommend some right now): " + description
+                }
+            ],
+            "temperature": 0.85,
+            "max_tokens": 512
+        })
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        }
+
+        const response = await fetch(apiURL, {
+            method: 'POST',
+            headers: headers,
+            body: payload
+        })
+
+        const respData = await response.json()
+
+        if (!response.ok) { 
+            throw new Error(response.statusText)
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(respData)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: error.message})
+        }
+    }
+}
