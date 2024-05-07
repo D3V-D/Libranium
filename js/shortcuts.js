b2437d8c45ad8da4850836fc3ad47ec9fb4a6fac
D3V-D <devendangi10@gmail.com>
2024-05-07 08:14:44 -0400

    Removed extraneous links and pages

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -48,26 +48,6 @@ const hotkeys = [
                     window.location.href = "/description"
                 }
             },
-            {
-                id: "ByTopic",
-                title: "Generate by Topics",
-                hotkey: "cmd+o",
-                mdIcon: "description",
-                keywords: "text, description, words, custom",
-                handler: () => {
-                    window.location.href = "/topics"
-                }
-            },
-            {
-                id: "ByLP",
-                title: "Generate by Lesson Plan",
-                hotkey: "cmd+l",
-                mdIcon: "school",
-                keywords: "text, description, words, custom",
-                handler: () => {
-                    window.location.href = "/LP"
-                }
-            }
         ]
     }
 ];
@@ -75,6 +55,5 @@ const ninja = document.querySelector("ninja-keys");
 ninja.data = hotkeys;
 
 searchbar.addEventListener("click", () => {
-    console.log("hi")
     ninja.open()
 })
